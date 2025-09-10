f5e298258a51477dc810f8842363b3ff53645a65
Alison Martins <alisonmarteso@gmail.com>
2025-09-10 19:16:42 -0300

    Primeiro commit

---
+++ src/main/java/com/grupo6/ConectaJob/ConectaJobApplication.java
@@ -0,0 +1,13 @@
+package com.grupo6.ConectaJob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectaJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo6/ConectaJob/ConectaJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo6.ConectaJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectaJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
